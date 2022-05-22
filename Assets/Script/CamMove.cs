fe70b0f6da79adb2896732615b822421ec798ac9
nuramyra <96581651+lildaizee@users.noreply.github.com>
2022-05-22 11:44:07 +0800

    feat: slow down hamburger

--- Assets/Script/CamMove.cs
+++ Assets/Script/CamMove.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class CamMove : MonoBehaviour
 {
+    public Transform player;
+    Vector3 offset;
+    Vector3 targetPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 5);
+        offset = transform.position - player.position;
+        //GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        targetPos = player.position + offset;
+        targetPos.x = 0;
+        transform.position = targetPos;
     }
 }
