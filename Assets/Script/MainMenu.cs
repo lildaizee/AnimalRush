841b20e7f25d60c1ca77e36c249e76a1ba8cc8bb
AmirulIsmail <91870087+AmirulIsmail@users.noreply.github.com>
2022-05-23 23:15:21 +0800

    Stroytelling
    Buat storytelling scene

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -15,4 +15,9 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("Leaderboard");
     }
 
+    public void MainMenuScreen()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
 }
